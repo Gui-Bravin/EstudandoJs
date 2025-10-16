a3f54ce148bafab09d3f45baafc700cf9c8d14da
Guilherme Bravin <81032170+Gui-Bravin@users.noreply.github.com>
2025-10-16 18:16:58 +0000

    Adiciona os arquivos ex008 e o ex010 que explica um pouco sobre condições (IF)

---
+++ ex008.js
@@ -0,0 +1,15 @@
+console.log("o console funcionou corretamente");
+var vel = 78.2;
+console.log(`A sua velocidade é de ${vel}`);
+if (vel > 60) {
+  console.log("A sua velocidade esta acima da maxima. Multado!");
+} else {
+  console.log("Uso sempre o cinto de segurança!");
+}
+var pais = "EUA";
+console.log(`vivendo no Pais ${pais}`);
+if (pais == "brasil") {
+  console.log("Brasileiro!");
+} else {
+  console.log("Gringo!");
+}
