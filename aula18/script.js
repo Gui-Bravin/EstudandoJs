5764296724aab69dc020aff1dd16b444513d8e3d
Guilherme Bravin <81032170+Gui-Bravin@users.noreply.github.com>
2025-10-24 03:59:45 +0000

    algumas melhorias no  ex da aula 18

--- aula18/script.js
+++ aula18/script.js
@@ -6,9 +6,12 @@ function carregar() {
   msg.innerHTML = `agora são ${hora} horas.`;
   if (hora >= 0 && hora < 12) {
     img.src = "manha.jpg";
+    document.body.style.background = "#f0c25eff";
   } else if (hora >= 12 && hora < 18) {
     img.src = "tarde.jpg";
+    document.body.style.background = "#b87d66ff";
   } else {
     img.src = "noite.jpg";
+    document.body.style.background = "#67496dff";
   }
 }
